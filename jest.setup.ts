8ae9c93fc86681d92ff427397267ef7e3663960e
Liam Adye <liam.adye@gmail.com>
2026-01-31 16:23:50 +1100

    docs: add comments to jest setup and tsconfig

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,10 +1,16 @@
+/**
+ * Jest setup file for the test environment.
+ * This file is executed before each test and is used to configure
+ * global mocks and testing-library extensions.
+ */
 import '@testing-library/jest-dom'
 import React from 'react'
 
-// Mock next/image
+// Mock next/image to handle it in a testing environment without a Next.js server
 jest.mock('next/image', () => ({
     __esModule: true,
     default: (props: any) => {
+        // eslint-disable-next-line @next/next/no-img-element
         return React.createElement('img', props)
     },
 }))
